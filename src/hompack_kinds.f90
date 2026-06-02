53ed9a304e99e6d56192d08eac6726b1772e9c6d
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2026-06-02 22:40:36 +0200

    feat: add hompack_kinds module for real kinds and numeric constants

---
+++ src/hompack_kinds.f90
@@ -0,0 +1,17 @@
+module hompack_kinds
+!! Real kinds and common numeric constants.
+
+   use iso_fortran_env, only: real64
+   implicit none
+   private
+
+   public :: dp
+   public :: zero, one, pi
+
+   integer, parameter :: dp = real64
+
+   real(dp), parameter :: zero = 0.0_dp
+   real(dp), parameter :: one = 1.0_dp
+   real(dp), parameter :: pi = 4*atan(one)
+
+end module hompack_kinds
